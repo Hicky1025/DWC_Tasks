4cca4ce7589d779354a7335631f72733b4d00a6e
Hicky1025 <plane1025.ict@gmail.com>
2022-11-18 14:24:33 +0900

    Append assignment content to file

--- omikuji.js
+++ omikuji.js
@@ -3,6 +3,10 @@ let username;
 let userresult;
 
 username = prompt("お名前を教えて下さい。");
+if (username == "") {
+    username = "名無し";
+}
+// htmlへの出力命令の前に特定条件でのusernameの書き換えを実施
 document.getElementById("name").innerHTML = username;
 
 let rand = Math.floor( Math.random() * 5);
